e4b211aa11801daf4866f411bd21cc264b76489e
Jake Bromberg <jake.bromberg@gmail.com>
2012-10-19 11:19:52 -0400

    converting project

---
+++ Classes/IndefinitelySpinningAnimation.h
@@ -0,0 +1,15 @@
+//
+//  IndefinitelySpinningAnimation.h
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 6/27/12.
+//  Copyright (c) 2012 WXYC. All rights reserved.
+//
+
+#import <QuartzCore/QuartzCore.h>
+
+@interface IndefinitelySpinningAnimation : CAAnimation
+
++ (id)getAnimation;
+
+@end
---
+++ Classes/IndefinitelySpinningAnimation.m
@@ -0,0 +1,29 @@
+//
+//  IndefinitelySpinningAnimation.m
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 6/27/12.
+//  Copyright (c) 2012 WXYC. All rights reserved.
+//
+
+#import "IndefinitelySpinningAnimation.h"
+
+@implementation IndefinitelySpinningAnimation
+
++ (id)getAnimation {
+	CABasicAnimation *spinAnimation;
+	
+	spinAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+	spinAnimation.duration = 2;
+	spinAnimation.repeatCount = FLT_MAX;
+	spinAnimation.autoreverses = NO;
+	spinAnimation.fromValue = @(M_PI / 3);
+	spinAnimation.toValue = @0.0f;
+	spinAnimation.removedOnCompletion = NO;
+	
+	[spinAnimation retain];
+	
+	return spinAnimation;
+}
+
+@end
