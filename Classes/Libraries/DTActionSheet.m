81d1703c52f5fe9b9eaf2834d9c2114ad30d4037
Jake Bromberg <jake.bromberg@gmail.com>
2012-12-29 21:36:29 -0500

    reimplemented NP protocols
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

---
+++ Classes/Libraries/DTActionSheet.m
@@ -0,0 +1,31 @@
+//
+//  DTActionSheet.m
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 12/27/12.
+//  Copyright (c) 2012 WXYC. All rights reserved.
+//
+
+#import "DTActionSheet.h"
+
+@implementation DTActionSheet
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Classes/NextPreviousItemProtocol.h
@@ -0,0 +1,13 @@
+//
+//  NextPreviousItemProtocol.h
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 12/27/12.
+//  Copyright (c) 2012 WXYC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol NextPreviousItemProtocol <NSObject>
+
+@end
