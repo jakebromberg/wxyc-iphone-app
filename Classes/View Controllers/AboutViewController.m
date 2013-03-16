752f0bc834e0f28a8cbe8655ce27ee4d459161f3
Jake Bromberg <jake.bromberg@gmail.com>
2013-03-16 17:43:22 -0400

    about view now works
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

--- Classes/View Controllers/AboutViewController.m
+++ Classes/View Controllers/AboutViewController.m
@@ -7,28 +7,31 @@
 
 @implementation AboutViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
 	[super viewDidLoad];
 	
 	NSString* aboutTemplatePath = [[NSBundle mainBundle] pathForResource:@"about" ofType:@"html"];
 	NSURL* url = [NSURL fileURLWithPath:aboutTemplatePath];
 	NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
 	
-	[(UIWebView *)self.view setDelegate:self];
 	[(UIWebView *)self.view loadRequest:requestObj];
 }
 
-- (void)webViewDidStartLoad:(UIWebView *)webView {
+- (void)webViewDidStartLoad:(UIWebView *)webView
+{
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
 }
 
-- (void)webViewDidFinishLoad:(UIWebView *)webView {
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
 	[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
 }
 
-- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
-	NSLog(@"expected:%d, got:%d", UIWebViewNavigationTypeLinkClicked, navigationType);
-	if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+	if (navigationType == UIWebViewNavigationTypeLinkClicked)
+	{
 		AboutViewController *webViewController = [[AboutViewController alloc] init];
 		
 		[[self navigationController] pushViewController:webViewController animated:YES];
