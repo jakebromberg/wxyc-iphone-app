f12b7332dfbf0f9b2122cc224fd2c0a884d89d6c
Jake Bromberg <jake.bromberg@gmail.com>
2013-10-07 16:26:37 -0700

    refactored spinning view animation
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

---
+++ Classes/Categories/UIView+Spin.h
@@ -0,0 +1,16 @@
+//
+//  UIView+Spin.h
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 9/13/13.
+//  Copyright (c) 2013 WXYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Spin)
+
+- (void)startSpin;
+- (void)stopSpin;
+
+@end
---
+++ Classes/Views/CassetteReelView.h
@@ -0,0 +1,13 @@
+//
+//  CassetteReelView.h
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 9/22/13.
+//  Copyright (c) 2013 WXYC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CassetteReelView : UIImageView
+
+@end
