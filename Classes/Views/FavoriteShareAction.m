3b81ded9fb7ff772767f34d19ee0740dd4baf6a4
Jake Bromberg <jake.bromberg@gmail.com>
2013-10-15 15:57:02 -0700

    updating to reflect changes in WebViewController

--- Classes/Views/FavoriteShareAction.m
+++ Classes/Views/FavoriteShareAction.m
@@ -9,18 +9,25 @@
 #import "FavoriteShareAction.h"
 #import "NSString+Additions.h"
 #import "WebViewController.h"
+#import "UIApplication+PresentViewController.h"
 
 @implementation FavoriteShareAction
 
 + (void)sharePlaycut:(Playcut *)playcut
 {
+    NSURLRequest *request = [FavoriteShareAction urlRequestForPlaycut:playcut];
+	WebViewController *webViewController = [[WebViewController alloc] initWithRequest:request];
+    [UIApplication presentViewController:webViewController animated:YES completion:nil];
+}
+
++ (NSURLRequest *)urlRequestForPlaycut:(Playcut *)playcut
+{
 	NSString *artist = [[playcut valueForKey:@"artist"] urlEncodeUsingEncoding:NSUTF8StringEncoding];
 	NSString *song = [[playcut valueForKey:@"song"] urlEncodeUsingEncoding:NSUTF8StringEncoding];
-	
 	NSString *url = [@"http://google.com/search?q=%@+%@" formattedWith:@[artist, song]];
-	WebViewController *webViewController = [[WebViewController alloc] init];
-	[[UIApplication sharedApplication].delegate.window.rootViewController presentViewController:webViewController animated:YES completion:nil];
-	[webViewController loadURL:[NSURL URLWithString:url]];
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:url]];
+    
+    return request;
 }
 
 @end
