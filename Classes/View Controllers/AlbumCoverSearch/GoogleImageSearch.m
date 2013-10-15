bc256a1c950bba2c5c05108af4b062971ca3309e
Jake Bromberg <jake.bromberg@gmail.com>
2013-10-15 15:57:27 -0700

    refactored operationsin GoogleImageSearch

--- Classes/View Controllers/AlbumCoverSearch/GoogleImageSearch.m
+++ Classes/View Controllers/AlbumCoverSearch/GoogleImageSearch.m
@@ -14,15 +14,7 @@ static NSString *IMAGE_SEARCH_URL = @"https://ajax.googleapis.com/ajax/services/
 static const NSString *LARGE_IMG_SIZE = @"large";
 static const NSString *API_KEY = @"ABQIAAAA5dyU_ZOZxVJ-rCQOTnH3khTF4zxbv1moelZ6wxYzrId3_vCc7hSxiVhd0OeM4oTlndTkE3v2ankvuA";
 
-typedef void(^OperationUnit)(id *accumulator, NSError **error);
-
-
-@interface GoogleImageSearch ()
-
-+ (NSURLRequest *)requestForKeywords:(NSArray *)keywords;
-
-@end
-
+typedef id(^OperationUnit)(id accumulator, NSError **error);
 
 @implementation GoogleImageSearch
 
@@ -34,81 +26,59 @@ typedef void(^OperationUnit)(id *accumulator, NSError **error);
 	dispatch_async(queue, block);
 }
 
++ (NSArray *)operationBlocks
+{
+    return @[
+		^(NSArray *keywords, NSError **error) {
+			NSString *query = [[keywords join:@"+"] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+			NSString *urlString = [IMAGE_SEARCH_URL formattedWith:@[LARGE_IMG_SIZE, API_KEY, query]];
+			NSURL *URL = [NSURL URLWithString:urlString];
+			
+			return [NSURLRequest requestWithURL:URL];
+		},
+
+		^(NSURLRequest *accumulator, NSError **error) {
+			return [NSURLConnection sendSynchronousRequest:accumulator returningResponse:nil error:error];
+		},
+
+		^(NSData *accumulator, NSError **error) {
+			return [NSJSONSerialization JSONObjectWithData:accumulator options:NSJSONReadingAllowFragments error:error];
+		},
+
+		^(NSDictionary *accumulator, NSError **error) {
+			return [accumulator valueForKeyPath:@"responseData.results.url"];
+		},
+
+		^(id accumulator, NSError **error) {
+			return [accumulator firstObject];
+		},
+
+		^(id accumulator, NSError **error) {
+			return [accumulator stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+		},
+	];
+}
+
 dispatch_block_t (^fetchData)(NSArray *, OperationHandler) = ^(NSArray *keywords, OperationHandler operationHandler)
 {
 	return ^{
-		id operationBlocks = @[
-			^(id *accumulator, NSError **error) {
-			  *accumulator = [GoogleImageSearch requestForKeywords:keywords];
-			},
-			
-			^(id *accumulator, NSError **error) {
-			  *accumulator = [NSURLConnection sendSynchronousRequest:*accumulator returningResponse:nil error:error];
-			},
 
-			^(id *accumulator, NSError **error) {
-			  *accumulator = [NSJSONSerialization JSONObjectWithData:*accumulator options:NSJSONReadingAllowFragments error:error];
-			},
-
-			^(id *accumulator, NSError **error) {
-				*accumulator = (*accumulator)[@"responseData"];
-				if (!*accumulator || ([NSNull null] == *accumulator))
-					*error = [NSError errorWithDomain:@"" code:-1 userInfo:nil];
-			},
-
-			^(id *accumulator, NSError **error) {
-				*accumulator = (*accumulator)[@"results"];
-				if ([*accumulator count] == 0)
-					*error = [NSError errorWithDomain:@"" code:@"" userInfo:nil];
-			},
-
-			^(id *accumulator, NSError **error) {
-				*accumulator = (*accumulator)[0];
-				if (!*accumulator || ([NSNull null] == *accumulator))
-					*error = [NSError errorWithDomain:@"" code:@"" userInfo:nil];
-			},
-			
-			^(id *accumulator, NSError **error) {
-				*accumulator = (*accumulator)[@"url"];
-				if (!*accumulator || ([NSNull null] == *accumulator))
-					*error = [NSError errorWithDomain:@"" code:@"" userInfo:nil];
-			},
-			 
-			^(id *accumulator, NSError **error) {
-				*accumulator = [*accumulator stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-				if (!*accumulator)
-					*error = [NSError errorWithDomain:@"" code:@"" userInfo:nil];
-			},
-			
-
-			^(id *accumulator, NSError **error) {
-				operationHandler([NSURL URLWithString:*accumulator], *error);
-			}
-		];
-
-		__block id accumulator;
+		__block __strong id accumulator = keywords;
 		__block NSError *error;
 
-		for(OperationUnit block in operationBlocks)
+		for(OperationUnit block in [GoogleImageSearch operationBlocks])
 		{
-			block(&accumulator, &error);
+			accumulator = block(accumulator, &error);
 			
-			if (error)
+			if (error || !accumulator || ([NSNull null] == accumulator))
 			{
-				operationHandler(nil, error);
-				break;
+				operationHandler(nil, error ?: [NSError errorWithDomain:@"" code:1 userInfo:nil]);
+				return;
 			}
 		}
+        
+        operationHandler([NSURL URLWithString:accumulator], error);
 	};
 };
-
-+ (NSURLRequest *)requestForKeywords:(NSArray *)keywords
-{
-	NSString *query = [[keywords join:@"+"] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-	NSString *urlString = [IMAGE_SEARCH_URL formattedWith:@[LARGE_IMG_SIZE, API_KEY, query]];
-	NSURL *URL = [NSURL URLWithString:urlString];
-	
-	return [NSURLRequest requestWithURL:URL];
-}
 
 @end
