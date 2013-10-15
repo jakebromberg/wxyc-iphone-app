f4022b5175ed0182f4f1a618621f776ef5be69b6
Jake Bromberg <jake.bromberg@gmail.com>
2013-10-14 21:25:56 -0700

    it's the return of the WXYCDataStack
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

---
+++ Classes/Models/XYCDataStack.h
@@ -0,0 +1,14 @@
+//
+//  XYCDataStack.h
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 10/13/13.
+//  Copyright (c) 2013 WXYC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "NSObject+Singleton.h"
+
+@interface XYCDataStack : NSObject <XYCSingleton>
+
+@end
---
+++ Classes/Models/XYCDataStack.m
@@ -0,0 +1,83 @@
+//
+//  XYCDataStack.m
+//  WXYCapp
+//
+//  Created by Jake Bromberg on 10/13/13.
+//  Copyright (c) 2013 WXYC. All rights reserved.
+//
+
+#import "XYCDataStack.h"
+#import <RestKit/RestKit.h>
+#import "Playcut.h"
+
+@interface NSManagedObjectContext ()
+
++ (void)MR_setRootSavingContext:(NSManagedObjectContext *)context;
++ (void)MR_setDefaultContext:(NSManagedObjectContext *)moc;
+
+@end
+
+@implementation XYCDataStack 
+
+- (id)init
+{
+	self = [super init];
+	
+	if (!self)
+		return nil;
+	
+	[[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationWillTerminateNotification object:[UIApplication sharedApplication] queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *note)
+	{
+		[self tidyUpCoreData];
+	}];
+	
+	[self configureCoreData];
+	[self tidyUpCoreData];
+	
+	return self;
+}
+
+- (void)configureCoreData
+{
+    RKManagedObjectStore *managedObjectStore = [self createManagedObjectStore];
+	[self configureManagedObjectStore:managedObjectStore];
+	[self configureMagicalRecordWithManagedObjectStore:managedObjectStore];
+	
+	RKObjectManager *objectManager = [RKObjectManager managerWithBaseURL:[NSURL URLWithString:@"http://wxyc.info/"]];
+    objectManager.managedObjectStore = managedObjectStore;
+}
+
+- (RKManagedObjectStore *)createManagedObjectStore
+{
+	NSURL *modelURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"Playlist2" ofType:@"momd"]];
+    NSManagedObjectModel *managedObjectModel = [[[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL] mutableCopy];
+	
+    return [[RKManagedObjectStore alloc] initWithManagedObjectModel:managedObjectModel];
+}
+
+- (void)configureManagedObjectStore:(RKManagedObjectStore *)managedObjectStore
+{
+    NSString *storePath = [RKApplicationDataDirectory() stringByAppendingPathComponent:@"WXYC78.sqlite"];
+    NSError *error = nil;
+    [managedObjectStore addSQLitePersistentStoreAtPath:storePath fromSeedDatabaseAtPath:nil withConfiguration:nil options:nil error:&error];
+    [managedObjectStore createManagedObjectContexts];
+}
+
+- (void)configureMagicalRecordWithManagedObjectStore:(RKManagedObjectStore *)managedObjectStore
+{
+    [NSPersistentStoreCoordinator MR_setDefaultStoreCoordinator:managedObjectStore.persistentStoreCoordinator];
+    [NSManagedObjectContext MR_setRootSavingContext:managedObjectStore.persistentStoreManagedObjectContext];
+    [NSManagedObjectContext MR_setDefaultContext:managedObjectStore.mainQueueManagedObjectContext];
+}
+
+- (void)tidyUpCoreData
+{
+	//Clean out unfavorited data
+	for (Playcut *playcut in [Playcut findByAttribute:@"Favorite" withValue:@NO]) {
+		[playcut deleteEntity];
+	}
+	
+	[[NSManagedObjectContext defaultContext] saveToPersistentStoreAndWait];
+}
+
+@end
