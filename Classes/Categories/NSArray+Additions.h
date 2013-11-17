904020b95dd6a1fa0c3911d78143d54bf9384ee8
Jake Bromberg <jake.bromberg@gmail.com>
2013-11-16 17:56:39 -0800

    added several functions to NSArray+Additions
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

--- Classes/Categories/NSArray+Additions.h
+++ Classes/Categories/NSArray+Additions.h
@@ -8,9 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+typedef id(^NSArrayMapBlock) (id obj, NSUInteger idx);
+typedef id(^NSArrayFilterBlock) (id obj, NSUInteger, BOOL *stop);
+
 @interface NSArray (Additions)
 
 - (NSString *)join:(NSString *)glue;
 - (NSString *)join;
+
+- (instancetype)map:(NSArrayMapBlock)mapBlock;
+- (instancetype)filter:(NSArrayFilterBlock)filterBlock;
+
+- (id)objectPassingTest:(BOOL(^)(id obj))test;
 
 @end
--- Classes/Categories/NSArray+Additions.m
+++ Classes/Categories/NSArray+Additions.m
@@ -20,4 +20,42 @@
 	return [self join:@""];
 }
 
+- (instancetype)map:(NSArrayMapBlock)mapBlock
+{
+	NSMutableArray *mappedArray = [self mutableCopy];
+	
+	[self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+		[mappedArray addObject:mapBlock(obj, obj)];
+	}];
+	
+	return mappedArray;
+}
+
+- (instancetype)filter:(NSArrayFilterBlock)filterBlock
+{
+	NSMutableArray *filteredArray = [NSMutableArray array];
+	
+	[self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
+		BOOL passesTest = filterBlock(obj, idx, stop);
+		
+		if (passesTest) {
+			[filteredArray addObject:obj];
+		}
+	}];
+	
+	return filteredArray;
+}
+
+- (id)objectPassingTest:(BOOL(^)(id obj))test
+{
+	NSUInteger idx = [self indexOfObjectPassingTest:^BOOL(id obj, NSUInteger idx, BOOL *stop) {
+		return test(obj);
+	}];
+	
+	if (idx == NSNotFound)
+		return nil;
+	else
+		return [self objectAtIndex:idx];
+}
+
 @end
