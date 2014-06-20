18c46b6470573534fa5e03acb4dc4e010da13701
Jake Bromberg <jake.bromberg@gmail.com>
2014-06-20 09:19:34 -0700

    Adding today extension to app
    Signed-off-by: Jake Bromberg <jake.bromberg@gmail.com>

---
+++ WXYC Today Extension/TodayViewController.m
@@ -0,0 +1,46 @@
+//
+//  TodayViewController.m
+//  WXYC Today Extension
+//
+//  Created by Jake Bromberg on 6/20/14.
+//  Copyright (c) 2014 WXYC. All rights reserved.
+//
+
+#import "TodayViewController.h"
+#import <NotificationCenter/NotificationCenter.h>
+
+@interface TodayViewController () <NCWidgetProviding>
+
+@end
+
+@implementation TodayViewController
+
+- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler {
+    // Perform any setup necessary in order to update the view.
+    
+    // If an error is encoutered, use NCUpdateResultFailed
+    // If there's no update required, use NCUpdateResultNoData
+    // If there's an update, use NCUpdateResultNewData
+
+    completionHandler(NCUpdateResultNewData);
+}
+
+@end
