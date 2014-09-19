150a899d14de7ff646aece77431317c4fc399bd3
Jake Bromberg <jake.bromberg@gmail.com>
2014-09-19 15:19:41 -0700

    restoring classic cell appearance

--- Classes/LivePlaylistTableViewCell.m
+++ Classes/LivePlaylistTableViewCell.m
@@ -26,33 +26,39 @@
 
 - (void)awakeFromNib
 {
-//	self.clipsToBounds = NO;
-//	self.layer.masksToBounds = NO;
+	self.clipsToBounds = NO;
+	self.layer.masksToBounds = NO;
 
-//	[self.layer insertSublayer:self.shadowLayer atIndex:0];
-	
-	[self observeKeyPath:@keypath(self.containerView, frame) changeBlock:^(id change) {
-		self.containerView.frame = _containerView.frame;
-		[self.containerView setNeedsDisplay];
+    self.containerView.layer.borderColor = [UIColor colorWithWhite:.70f alpha:1.f].CGColor;
+    self.containerView.layer.borderWidth = 1.f;
+    self.containerView.layer.cornerRadius = 5.f;
+    self.containerView.layer.masksToBounds = YES;
+    
+    [self.contentView.layer insertSublayer:self.shadowLayer atIndex:0];
+
+    [self observeKeyPath:@keypath(self.containerView.layer, frame) changeBlock:^(NSDictionary *change) {
+        _shadowLayer.shadowPath = CGPathCreateWithRoundedRect(self.containerView.frame, 5.f, 5.f, &CGAffineTransformIdentity);
 	}];
 }
 
 - (CALayer *)shadowLayer
 {
-	CALayer *shadowLayer = [CALayer layer];
-	shadowLayer.frame = self.containerView.frame;
-	shadowLayer.backgroundColor = [UIColor clearColor].CGColor;
-	shadowLayer.shadowColor = [UIColor colorWithWhite:.75f alpha:1.f].CGColor;
-	shadowLayer.shadowOffset = (CGSize){-.5f, 2.5f};
-	shadowLayer.shadowRadius = 1.5f;
-	shadowLayer.shadowOpacity = 1;
-	shadowLayer.shadowPath = CGPathCreateWithRoundedRect(self.containerView.bounds, 5.f, 5.f, &CGAffineTransformIdentity);
-	shadowLayer.cornerRadius = 5.f;
-	shadowLayer.masksToBounds = NO;
-	shadowLayer.shouldRasterize = YES;
-	shadowLayer.rasterizationScale = [UIScreen mainScreen].scale;
-
-	return shadowLayer;
+    if (!_shadowLayer)
+    {
+        _shadowLayer = [CALayer layer];
+        _shadowLayer.frame = self.containerView.bounds;
+        _shadowLayer.backgroundColor = [UIColor clearColor].CGColor;
+        _shadowLayer.shadowColor = [UIColor colorWithWhite:.75f alpha:1.f].CGColor;
+        _shadowLayer.shadowOffset = (CGSize){-.5f, 1.f};
+        _shadowLayer.shadowRadius = 1.5f;
+        _shadowLayer.shadowOpacity = 1;
+        _shadowLayer.shadowPath = CGPathCreateWithRoundedRect(self.containerView.bounds, 5.f, 5.f, &CGAffineTransformIdentity);
+        _shadowLayer.masksToBounds = NO;
+        _shadowLayer.shouldRasterize = YES;
+        _shadowLayer.rasterizationScale = [UIScreen mainScreen].scale;
+    }
+    
+	return _shadowLayer;
 }
 
 @end
