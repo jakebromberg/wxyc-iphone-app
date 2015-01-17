a27deb33521d421262f11012d8793e5511187b6e
Jake Bromberg <jake.bromberg@gmail.com>
2015-01-17 10:13:58 -0800

    fixing selected/unselected assets and sprucing up the syntax

--- Classes/View Controllers/WXYCTabBarControllerViewController.m
+++ Classes/View Controllers/WXYCTabBarControllerViewController.m
@@ -10,25 +10,51 @@
 
 static const UIEdgeInsets insets = (UIEdgeInsets){6, 0, -6, 0};
 
+typedef NS_ENUM(NSUInteger, XYCTabBarItems) {
+    XYCLivePlaylistItem,
+    XYCFavoritesItem,
+    XYCInfoItem
+};
+
+
+@interface WXYCTabBarControllerViewController ()
+
+@property (nonatomic, readonly) UITabBarItem *livePlaylistItem;
+@property (nonatomic, readonly) UITabBarItem *favoritesItem;
+@property (nonatomic, readonly) UITabBarItem *infoItem;
+
+@end
+
+
 @implementation WXYCTabBarControllerViewController
 
 - (void)viewWillAppear:(BOOL)animated
 {
 	[super viewWillAppear:animated];
 	
-	UITabBar *tabBar = self.tabBar;
+	self.livePlaylistItem.imageInsets = insets;
+    self.livePlaylistItem.image = [[UIImage imageNamed:@"tabbar-item-playlist-unselected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.livePlaylistItem.selectedImage = [[UIImage imageNamed:@"tabbar-item-playlist-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
 	
-	[tabBar.items[0] setImageInsets:insets];
-    [tabBar.items[0] setImage:[[UIImage imageNamed:@"tabbar-item-playlist-selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-    [tabBar.items[0] setSelectedImage:[[UIImage imageNamed:@"tabbar-item-playlist-unselected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-	
-	[tabBar.items[1] setImageInsets:insets];
-    [tabBar.items[1] setImage:[[UIImage imageNamed:@"tabbar-item-favorites-selected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-    [tabBar.items[1] setSelectedImage:[[UIImage imageNamed:@"tabbar-item-favorites-unselected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-	
-	[tabBar.items[2] setImageInsets:insets];
-    [tabBar.items[2] setImage:[[UIImage imageNamed:@"tabbar-item-info-selected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
-    [tabBar.items[2] setSelectedImage:[[UIImage imageNamed:@"tabbar-item-info-unselected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]];
+	self.favoritesItem.imageInsets = insets;
+    self.favoritesItem.image = [[UIImage imageNamed:@"tabbar-item-favorites-unselected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.favoritesItem.selectedImage = [[UIImage imageNamed:@"tabbar-item-favorites-selected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+
+    self.infoItem.imageInsets = insets;
+    self.infoItem.image = [[UIImage imageNamed:@"tabbar-item-info-unselected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    self.infoItem.selectedImage = [[UIImage imageNamed:@"tabbar-item-info-selected.png"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+- (UITabBarItem *)livePlaylistItem {
+    return self.tabBar.items[XYCLivePlaylistItem];
+}
+
+- (UITabBarItem *)favoritesItem {
+    return self.tabBar.items[XYCFavoritesItem];
+}
+
+- (UITabBarItem *)infoItem {
+    return self.tabBar.items[XYCInfoItem];
 }
 
 @end
