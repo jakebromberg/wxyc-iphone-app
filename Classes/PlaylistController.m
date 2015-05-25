74d1f881161f38cd55e8a45ea59daf801bca22c4
Jake Bromberg <jake.bromberg@gmail.com>
2015-05-24 18:12:42 -0700

    removing boilerplate code

--- Classes/PlaylistController.m
+++ Classes/PlaylistController.m
@@ -15,7 +15,6 @@
 #import "Talkset.h"
 
 @interface PlaylistController()
-@interface PlaylistController ()
 
 @property (nonatomic, strong, readwrite) NSArray *playlistEntries;
 @property (nonatomic, readonly) NSDictionary *parameters;
