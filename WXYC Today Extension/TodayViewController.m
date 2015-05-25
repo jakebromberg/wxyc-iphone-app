2006408ee0a61ac2d4cf76feb50c116f5612a26c
Jake Bromberg <jake.bromberg@gmail.com>
2015-05-24 18:12:42 -0700

    removing boilerplate code

--- WXYC Today Extension/TodayViewController.m
+++ WXYC Today Extension/TodayViewController.m
@@ -24,25 +24,8 @@ static const NCUpdateResult XYCBackgroundFetchMappings[] =
 
 @end
 
+
 @implementation TodayViewController
-
-- (instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 - (void)widgetPerformUpdateWithCompletionHandler:(void (^)(NCUpdateResult))completionHandler
 {
